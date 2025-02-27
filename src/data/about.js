5402c943ab9e1848daec9efc1e6028fd2a429ed3
janvithummar <janvithummar76@gmail.com>
2025-02-27 16:04:50 +0530

    Design About Page

--- src/data/about.js
+++ src/data/about.js
@@ -1,6 +1,9 @@
 import person1 from "../assets/OurTeam/person1.png";
 import person2 from "../assets/OurTeam/person2.png";
 import person3 from "../assets/OurTeam/person3.png";
+import { CiDollar, CiShop } from "react-icons/ci";
+import { TbUsers } from "react-icons/tb";
+import { FaSackDollar } from "react-icons/fa6";
 
 export const teamMembers = [
     { name: "Tom Cruise", role: "Founder & Chairman", image: person1 },
@@ -8,4 +11,11 @@ export const teamMembers = [
     { name: "Will Smith", role: "Product Designer", image: person3 },
     { name: "Chris Evans", role: "Marketing Head", image: person1 },
     { name: "Scarlett Johansson", role: "HR Manager", image: person2 }
+  ];
+
+ export const stats = [
+    { Icon: CiShop, value: "10.5k", label: "Sellers active on our site" },
+    { Icon: CiDollar, value: "33k", label: "Monthly Product Sale" },
+    { Icon: TbUsers, value: "45.5k", label: "Customers active on our site" },
+    { Icon: FaSackDollar, value: "25k", label: "Annual gross sale on our site" },
   ];
---
+++ src/pages/About.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import about from "../assets/about.jpeg";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { stats, teamMembers } from "../data/about";
+
+const About = () => {
+  const [hoveredStateIndex, setHoveredStateIndex] = useState(null);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 2 } },
+      { breakpoint: 640, settings: { slidesToShow: 1 } },
+    ],
+  };
+
+  return (
+    <div className="flex flex-col items-center px-20 py-10">
+      <div className="text-gray-500 items-left w-full mt-10">Home / About</div>
+      <div className="flex flex-col md:flex-row items-center justify-between gap-25">
+        <div className="md:w-1/2 space-y-10">
+          <h2 className="text-3xl font-bold">Our Story</h2>
+          <p className="text-gray-700">
+            Launched in 2015, Exclusive is South Asia’s premier online shopping marketplace...
+          </p>
+          <p className="text-gray-700">
+            Exclusive has more than 1 Million products to offer, growing at a very fast pace...
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore mollitia dolorum tempora nobis, maiores velit fugit eius provident reiciendis eum aspernatur nisi ipsum. Harum perspiciatis vero minima incidunt, totam cum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus in, nisi voluptatibus doloribus, et, corporis saepe nobis sit rerum sint quasi repellendus eligendi laudantium deserunt natus doloremque veritatis! Consequuntur, accusamus?
+          </p>
+        </div>
+        <div className="md:w-1/2 flex justify-center mt-6">
+          <img src={about} alt="Shopping" className="rounded-lg shadow-lg w-full" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 w-full text-center py-20">
+        {stats.map((stat, index) => (
+          <div
+            key={index}
+            className={`p-10 border rounded-lg flex flex-col items-center shadow-lg transition duration-300 ${ hoveredStateIndex === index ? "bg-red-500 text-white" : "" }`}
+            onMouseEnter={() => setHoveredStateIndex(index)}
+            onMouseLeave={() => setHoveredStateIndex(null)}
+          >
+            <div className={`p-4 rounded-full ${ hoveredStateIndex === index ? "bg-red-300" : "bg-gray-300" }`}>
+              <stat.Icon className={`text-8xl p-6 rounded-full ${ hoveredStateIndex === index ? "bg-white text-black" : "text-white bg-black" }`} />
+            </div>
+            <p className="text-4xl font-bold mt-8">{stat.value}</p>
+            <p className="text-xl mt-3">{stat.label}</p>
+          </div>
+        ))}
+      </div>
+
+      <Slider {...settings} className="w-full mb-20">
+        {teamMembers.map((member, index) => (
+          <div key={index} className="text-center p-4">
+            <img
+                src={member.image}
+                alt={member.name}
+                className="rounded-lg shadow-lg w-full h-[550px] px-10 mx-auto"
+            />
+            <h3 className="text-xl font-bold mt-4">{member.name}</h3>
+            <p>{member.role}</p>
+            <div className="flex justify-center space-x-3 mt-2 text-gray-600">
+              <FaTwitter className="text-xl cursor-pointer hover:text-blue-500" />
+              <FaInstagram className="text-xl cursor-pointer hover:text-pink-500" />
+              <FaLinkedin className="text-xl cursor-pointer hover:text-blue-700" />
+            </div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default About;
